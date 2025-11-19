20cae88f655fbccdcecda61e920ae1cd34a7029e
Arun <arun010709@gmail.com>
2025-11-19 07:01:12 +0530

    Spring docker
    Spring docker

---
+++ spring-docker/src/main/java/com/docker/spring_docker/SpringDockerApplication.java
@@ -0,0 +1,21 @@
+package com.docker.spring_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringDockerApplication {
+
+	@GetMapping("/greeting")
+	public String greeting(){
+		return "Welcome to the dockerized spring boot app!!!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerApplication.class, args);
+	}
+
+}
